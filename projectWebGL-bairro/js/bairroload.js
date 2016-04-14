367d2783eeef4bc3832af542634590289ee44511
Gabriel <gabriel.vieira@ua.pt>
2016-04-14 14:48:52 +0100

    WebGL project

---
+++ projectWebGL-bairro/js/bairroload.js
@@ -0,0 +1,896 @@
+/*
+	WEBGL PROJECTO BAIRRO | 2015 | COMPUTAÇÃO VISUAL 
+	Elaborado por: Gabriel Vieira (68021) e Rui Oliveira (68779)
+*/
+
+//
+// Global Variables
+//
+
+var gl = null; // WebGL context
+
+var shaderProgram = null;
+
+var triangleVertexPositionBuffer = null;
+	
+var triangleVertexColorBuffer = null;
+
+// The GLOBAL transformation parameters
+
+var globalTz = 0.0;
+
+// The local transformation parameters
+
+// The translation vector
+
+var tx = 0.0;
+
+var ty = 0.0;
+
+var tz = 0.0;
+
+// The rotation angles in degrees
+
+var angleXX = 0.0;
+
+var angleYY = 0.0;
+
+var angleZZ = 0.0;
+
+// The scaling factors
+
+var sx = 0.5;
+
+var sy = 0.5;
+
+var sz = 0.5;
+
+// Local Animation controls
+
+var rotationXX_ON = 1;
+
+var rotationXX_DIR = 1;
+
+var rotationXX_SPEED = 1;
+ 
+var rotationYY_ON = 1;
+
+var rotationYY_DIR = 1;
+
+var rotationYY_SPEED = 1;
+ 
+var rotationZZ_ON = 1;
+
+var rotationZZ_DIR = 1;
+
+var rotationZZ_SPEED = 1;
+ 
+// To allow choosing the way of drawing the model triangles
+
+var primitiveType = null;
+ 
+// To allow choosing the projection type
+
+var projectionType = 1;  ///////// set to 1 to pespective projection to allow walk in the street
+
+// NEW --- Model Material Features
+
+// Ambient coef.
+
+var kAmbi = [ 0.2, 0.2, 0.2 ];
+
+// Difuse coef.
+
+var kDiff = [ 0.7, 0.7, 0.7 ];
+
+// Specular coef.
+
+var kSpec = [ 0.7, 0.7, 0.7 ];
+
+// Phong coef.
+
+var nPhong = 100;
+
+// Initial model has just ONE TRIANGLE
+
+
+var vertices = [
+
+		// FRONTAL TRIANGLE
+		 
+		-0.5, -0.5,  0.5,
+		 
+		 0.5, -0.5,  0.5,
+		 
+		 0.5,  0.5,  0.5,
+];
+
+var normals = [
+
+		// FRONTAL TRIANGLE
+		 
+		 0.0,  0.0,  1.0,
+		 
+		 0.0,  0.0,  1.0,
+		 
+		 0.0,  0.0,  1.0,
+];
+
+// Initial color values just for testing!!
+
+// They are to be computed by the Phong Illumination Model
+
+var colors = [
+
+		 // FRONTAL TRIANGLE
+		 	
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+		 
+		 1.00,  0.00,  0.00,
+];
+
+var textureCoords = [  // coordenadas da textura
+
+          // Front face
+          0.0, 0.0,
+          1.0, 0.0,
+          1.0, 1.0,
+          0.0, 1.0,
+
+          // Back face
+          1.0, 0.0,
+          1.0, 1.0,
+          0.0, 1.0,
+          0.0, 0.0,
+
+          // Top face
+          0.0, 1.0,
+          0.0, 0.0,
+          1.0, 0.0,
+          1.0, 1.0,
+
+          // Bottom face
+          1.0, 1.0,
+          0.0, 1.0,
+          0.0, 0.0,
+          1.0, 0.0,
+
+          // Right face
+          1.0, 0.0,
+          1.0, 1.0,
+          0.0, 1.0,
+          0.0, 0.0,
+
+          // Left face
+          0.0, 0.0,
+          1.0, 0.0,
+          1.0, 1.0,
+          0.0, 1.0,
+];
+
+//----------------------------------------------------------------------------
+//
+// The WebGL code
+//
+
+//----------------------------------------------------------------------------
+//
+//  Rendering
+//
+
+// handle texture adapted from classes
+function handleLoadedTexture(texture) {
+	
+	gl.bindTexture(gl.TEXTURE_2D, texture);
+	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
+	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+	gl.bindTexture(gl.TEXTURE_2D, null);
+}
+
+var webGLTexture;
+
+function initTexture() {
+	
+	webGLTexture = gl.createTexture();
+	webGLTexture.image = new Image();
+	webGLTexture.image.onload = function () {
+		handleLoadedTexture(webGLTexture)
+	}
+
+	webGLTexture.image.src = "House.png";
+}
+
+// Handling the Vertex and the Color Buffers
+function initBuffers() {	
+	
+	// Coordinates
+		
+	triangleVertexPositionBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexPositionBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	triangleVertexPositionBuffer.itemSize = 3;
+	triangleVertexPositionBuffer.numItems = vertices.length / 3;			
+
+	// Associating to the vertex shader
+	
+	gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, 
+			triangleVertexPositionBuffer.itemSize, 
+			gl.FLOAT, false, 0, 0);
+	
+	// Colors
+		
+	triangleVertexColorBuffer = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, triangleVertexColorBuffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+	triangleVertexColorBuffer.itemSize = 3;
+	triangleVertexColorBuffer.numItems = colors.length / 3;			
+
+	// Associating to the vertex shader
+	
+	gl.vertexAttribPointer(shaderProgram.vertexColorAttribute, 
+			triangleVertexColorBuffer.itemSize, 
+			gl.FLOAT, false, 0, 0);
+
+
+	// Textures
+		
+    cubeVertexTextureCoordBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
+ 	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
+    cubeVertexTextureCoordBuffer.itemSize = 2;
+    cubeVertexTextureCoordBuffer.numItems = 24;	
+
+}
+
+//----------------------------------------------------------------------------
+
+//  Computing the illumination and rendering the model
+
+function computeIllumination( mvMatrix ) {
+
+	// Phong Illumination Model
+	
+	// Clearing the colors array
+	
+	for( var i = 0; i < colors.length; i++ )
+	{
+		colors[i] = 0.0;
+	}
+	
+    // SMOOTH-SHADING 
+
+    // Compute the illumination for every vertex
+
+    // Iterate through the vertices
+    
+    for( var vertIndex = 0; vertIndex < vertices.length; vertIndex += 3 )
+    {	
+		// For every vertex
+		
+		// GET COORDINATES AND NORMAL VECTOR
+		
+		var auxP = vertices.slice( vertIndex, vertIndex + 3 );
+		
+		var auxN = normals.slice( vertIndex, vertIndex + 3 );
+
+        // CONVERT TO HOMOGENEOUS COORDINATES
+
+		auxP.push( 1.0 );
+		
+		auxN.push( 0.0 );
+		
+        // APPLY CURRENT TRANSFORMATION
+
+        var pointP = multiplyPointByMatrix( mvMatrix, auxP );
+
+        var vectorN = multiplyVectorByMatrix( mvMatrix, auxN );
+        
+        normalize( vectorN );
+
+		// VIEWER POSITION
+		
+		var vectorV = vec3();
+		
+
+		vectorV = symmetric( pointP ); // projectionType ==1
+
+		
+        normalize( vectorV );
+
+	    // Compute the 3 components: AMBIENT, DIFFUSE and SPECULAR
+	    
+	    // FOR EACH LIGHT SOURCE
+	    
+	    for(var l = 0; l < lightSources.length; l++ )
+	    {
+			if( lightSources[l].isOff() ) {
+				
+				continue;
+			}
+			
+	        // INITIALIZE EACH COMPONENT, with the constant terms
+	
+		    var ambientTerm = vec3();
+		
+		    var diffuseTerm = vec3();
+		
+		    var specularTerm = vec3();
+		
+		    // For the current light source
+		
+		    ambient_Illumination = lightSources[l].getAmbIntensity();
+		
+		    int_Light_Source = lightSources[l].getIntensity();
+		
+		    pos_Light_Source = lightSources[l].getPosition();
+		    
+		    // Animating the light source, if defined
+		    
+		    var lightSourceMatrix = mat4();
+		    
+		    // COMPLETE THE CODE FOR THE OTHER ROTATION AXES
+		    
+		    if( lightSources[l].isRotYYOn() ) 
+		    {
+				lightSourceMatrix = mult( 
+						lightSourceMatrix, 
+						rotationYYMatrix( lightSources[l].getRotAngleYY() ) );
+			}
+			
+	        for( var i = 0; i < 3; i++ )
+	        {
+			    // AMBIENT ILLUMINATION --- Constant for every vertex
+	   
+			    ambientTerm[i] = ambient_Illumination[i] * kAmbi[i];
+	
+	            diffuseTerm[i] = int_Light_Source[i] * kDiff[i];
+	
+	            specularTerm[i] = int_Light_Source[i] * kSpec[i];
+	        }
+	    
+	        // DIFFUSE ILLUMINATION
+	        
+	        var vectorL = vec4();
+	
+	        if( pos_Light_Source[3] == 0.0 )
+	        {
+	            // DIRECTIONAL Light Source
+	            
+	            vectorL = multiplyVectorByMatrix( 
+							lightSourceMatrix,
+							pos_Light_Source );
+	        }
+	        else
+	        {
+	            // POINT Light Source
+	
+	            // TO DO : apply the global transformation to the light source?
+	
+	            vectorL = multiplyPointByMatrix( 
+							lightSourceMatrix,
+							pos_Light_Source );
+				
+				for( var i = 0; i < 3; i++ )
+	            {
+	                vectorL[ i ] -= pointP[ i ];
+	            }
+	        }
+	
+			// Back to Euclidean coordinates
+			
+			vectorL = vectorL.slice(0,3);
+			
+	        normalize( vectorL );
+	
+	        var cosNL = dotProduct( vectorN, vectorL );
+	
+	        if( cosNL < 0.0 )
+	        {
+				// No direct illumination !!
+				
+				cosNL = 0.0;
+	        }
+	
+	        // SEPCULAR ILLUMINATION 
+	
+	        var vectorH = add( vectorL, vectorV );
+	
+	        normalize( vectorH );
+	
+	        var cosNH = dotProduct( vectorN, vectorH );
+	
+			// No direct illumination or viewer not in the right direction
+			
+	        if( (cosNH < 0.0) || (cosNL <= 0.0) )
+	        {
+	            cosNH = 0.0;
+	        }
+	
+	        // Compute the color values and store in the colors array
+	        
+	        var tempR = ambientTerm[0] + diffuseTerm[0] * cosNL + specularTerm[0] * Math.pow(cosNH, nPhong);
+	        
+	        var tempG = ambientTerm[1] + diffuseTerm[1] * cosNL + specularTerm[1] * Math.pow(cosNH, nPhong);
+	        
+	        var tempB = ambientTerm[2] + diffuseTerm[2] * cosNL + specularTerm[2] * Math.pow(cosNH, nPhong);
+	        
+			colors[vertIndex] += tempR;
+	        
+	        // Avoid exceeding 1.0
+	        
+			if( colors[vertIndex] > 1.0 ) {
+				
+				colors[vertIndex] = 1.0;
+			}
+	        
+	        // Avoid exceeding 1.0
+	        
+			colors[vertIndex + 1] += tempG;
+			
+			if( colors[vertIndex + 1] > 1.0 ) {
+				
+				colors[vertIndex + 1] = 1.0;
+			}
+			
+			colors[vertIndex + 2] += tempB;
+	        
+	        // Avoid exceeding 1.0
+	        
+			if( colors[vertIndex + 2] > 1.0 ) {
+				
+				colors[vertIndex + 2] = 1.0;
+			}
+	    }	
+	}
+}
+
+function drawModel( angleXX, angleYY, angleZZ, 
+					sx, sy, sz,
+					tx, ty, tz,
+					mvMatrix,
+					primitiveType ) {
+
+	// The the global model transformation is an input
+	
+	// Concatenate with the particular model transformations
+	
+    // Pay attention to transformation order !!
+    
+	mvMatrix = mult( mvMatrix, translationMatrix( tx, ty, tz ) );
+						 
+	mvMatrix = mult( mvMatrix, rotationZZMatrix( angleZZ ) );
+	
+	mvMatrix = mult( mvMatrix, rotationYYMatrix( angleYY ) );
+	
+	mvMatrix = mult( mvMatrix, rotationXXMatrix( angleXX ) );
+	
+	mvMatrix = mult( mvMatrix, scalingMatrix( sx, sy, sz ) );
+						 
+	// Passing the Model View Matrix to apply the current transformation
+	
+	var mvUniform = gl.getUniformLocation(shaderProgram, "uMVMatrix");
+	
+	gl.uniformMatrix4fv(mvUniform, false, new Float32Array(flatten(mvMatrix)));
+	
+	// NEW - Aux. Function for computing the illumination
+	
+	computeIllumination( mvMatrix );
+	
+	// Associating the data to the vertex shader
+
+	// Textures
+	gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
+    gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, cubeVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, webGLTexture);
+	// This can be done in a better way !!
+
+	initBuffers();
+	
+	// Drawing 
+	
+	// primitiveType allows drawing as filled triangles / wireframe / vertices
+	
+	if( primitiveType == gl.LINE_LOOP ) {
+		
+		// To simulate wireframe drawing!
+		
+		// No faces are defined! There are no hidden lines!
+		
+		// Taking the vertices 3 by 3 and drawing a LINE_LOOP
+		
+		var i;
+		
+		for( i = 0; i < triangleVertexPositionBuffer.numItems / 3; i++ ) {
+		
+			gl.drawArrays( primitiveType, 3 * i, 3 ); 
+		}
+	}	
+	else {
+				
+		gl.drawArrays(primitiveType, 0, triangleVertexPositionBuffer.numItems); 
+		
+	}	
+}
+
+// Handling mouse events
+
+// Adapted from www.learningwebgl.com
+
+
+var mouseDown = false;
+
+var lastMouseX = null;
+
+var lastMouseY = null;
+
+function handleMouseDown(event) {
+	
+    mouseDown = true;
+  
+    lastMouseX = event.clientX;
+  
+    lastMouseY = event.clientY;
+}
+
+function handleMouseUp(event) {
+
+    mouseDown = false;
+}
+
+
+
+function handleMouseMove(event) {
+
+    if (!mouseDown) {
+    	return;
+    } 
+  
+    // Rotation angles proportional to cursor displacement
+    
+    var newX = event.clientX;
+  
+    var newY = event.clientY;
+
+    var deltaX = newX - lastMouseX;
+    
+    angleYY += radians( 10 * deltaX  )
+
+    var deltaY = newY - lastMouseY;
+    
+    angleXX += radians( 10 * deltaY  )
+    
+    lastMouseX = newX
+    
+    lastMouseY = newY;
+  }
+
+
+//  Drawing the 3D scene
+
+function drawScene() {
+	
+	var pMatrix;
+	
+	var mvMatrix = mat4();
+	
+	// Clearing the frame-buffer and the depth-buffer
+	
+	gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+	
+	// A standard view volume.
+	
+	// Viewer is at (0,0,0)
+	
+	// Ensure that the model is "inside" the view volume
+	
+	pMatrix = perspective( 45, 1, 0.01, 5 );
+	
+	// Global transformation !!
+	
+	globalTz = -2.5;
+
+	// TO BE DONE !
+
+	
+	// Passing the Projection Matrix to apply the current projection
+	
+	var pUniform = gl.getUniformLocation(shaderProgram, "uPMatrix");
+	
+	gl.uniformMatrix4fv(pUniform, false, new Float32Array(flatten(pMatrix)));
+	
+	// GLOBAL TRANSFORMATION FOR THE WHOLE SCENE
+	
+	mvMatrix = translationMatrix( 0, 0, globalTz );
+
+	
+	// Instantianting the current model
+		
+	drawModel( angleXX, angleYY, angleZZ, 
+	           sx, sy, sz,
+	           tx, ty-0.5, tz,
+	           mvMatrix,
+	           primitiveType );
+}
+
+
+function tick() {
+	
+	requestAnimFrame(tick);
+	
+	drawScene();	
+}
+
+
+
+
+//----------------------------------------------------------------------------
+//
+//  User Interaction
+//
+
+function outputInfos(){
+    
+}
+
+//----------------------------------------------------------------------------
+
+
+
+var currentlyPressedKeys = {};
+function setEventListeners(canvas){
+	
+	// Adapted from:
+	
+	// http://stackoverflow.com/questions/23331546/how-to-use-javascript-to-read-local-text-file-and-read-line-by-line
+	canvas.onmousedown = handleMouseDown;
+    
+    document.onmouseup = handleMouseUp;
+    
+    document.onmousemove = handleMouseMove;
+    
+	// From learningwebgl.com
+
+    function handleKeyDown(event) {
+		
+        currentlyPressedKeys[event.keyCode] = true;
+    }
+
+    function handleKeyUp(event) {
+		
+        currentlyPressedKeys[event.keyCode] = false;
+    }
+
+	document.onkeydown = handleKeyDown;
+    
+    document.onkeyup = handleKeyUp;
+
+    //document.onkeypress = handleKeyPress;
+
+    document.addEventListener("keypress", function(event){
+  	var key =  event.keyCode || event.charCode; // ASCII
+    // console.log(key); 
+    switch(key){
+        case 115:
+        case 40:
+        	tz -= 0.05; // deslocar cena para o observador (andar em frente)
+        break;
+
+        case 38: 
+        case 119 : 
+        	tz += 0.05;  // afastar cena do observador (andar trás)
+        break;
+
+        case 37: 
+        case 97:
+        	angleYY -= 5; 
+        break;
+		
+		case 39: 
+        case 100:
+        	angleYY += 5;
+        break;	
+      }
+   
+    });
+	
+}
+
+/*
+	OBJ file read automatic 
+	Adaptado http://stackoverflow.com/questions/247483/http-get-request-in-javascript  
+*/
+
+function readTextFile(file){
+    var rawFile = new XMLHttpRequest();
+    var res = "";
+
+    rawFile.open("GET", file, false);
+            
+    rawFile.onreadystatechange = function (){
+        if(rawFile.readyState === 4){
+            if(rawFile.status === 200 || rawFile.status == 0){
+            	res = rawFile.responseText;
+            }
+        }
+    }
+
+    rawFile.send(null);
+    return res;
+}
+
+
+// inicializacao automatica do ficheiro OBJ
+function initOBJ(path){
+	
+	var lines = readTextFile(path).split('\n');
+	
+
+	// The new vertices
+	var newVertices = [];
+	
+	// The new normal vectors
+	var newNormals = [];
+	
+	// Check every line and store 
+
+	for(var line = 0; line < lines.length; line++){
+
+		// The tokens/values in each line
+
+	    // Separation between tokens is 1 or mode whitespaces
+
+	    var tokens = lines[line].split(/\s\s*/);
+	    
+	    // Array of tokens; each token is a string
+	    
+	    if( tokens[0] == "v" ){
+			// For every vertex we have 3 floating point values
+	
+		    for( j = 1; j < 4; j++ ) {
+			
+				newVertices.push( parseFloat( tokens[ j ] ) );
+			}
+		}
+
+	    if( tokens[0] == "vn" ) 
+	    {
+			// For every normal we have 3 floating point values
+	
+		    for( j = 1; j < 4; j++ ) {
+			
+				newNormals.push( parseFloat( tokens[ j ] ) );
+			}
+		}
+	}	
+				
+	// Assigning to the current model
+	
+	vertices = newVertices.slice();
+	
+	normals = newNormals.slice();
+	
+	// Checking to see if the normals are defined on the file
+	
+	if( normals.length == 0 )
+	{
+		computeVertexNormals( vertices, normals );
+	}
+				
+	// To render the model just read
+
+	initBuffers();
+
+	// RESET the transformations - NEED AUXILIARY FUNCTION !!
+	
+	tx = ty = tz = 0.0;
+				
+	angleXX = angleYY = angleZZ = 0.0;
+	
+	sx = sy = sz = 0.7;
+
+}
+
+//-------------------------------------------------------
+// WebGL Initialization
+
+function initWebGL( canvas ) {
+	try {
+		
+		// Create the WebGL context
+		
+		// Some browsers still need "experimental-webgl"
+		
+		gl = canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+		
+		// DEFAULT: The viewport occupies the whole canvas 
+		
+		// DEFAULT: The viewport background color is WHITE
+		
+		// NEW - Drawing the triangles defining the model
+		
+		primitiveType = gl.TRIANGLES;
+		
+		// DEFAULT: Face culling is DISABLED
+		
+		// DEFAULT: The BACK FACE is culled!!
+		
+		gl.cullFace( gl.BACK );
+
+		// Enable DEPTH-TEST
+		
+		gl.enable( gl.DEPTH_TEST );
+
+	} catch (e) {
+	}
+	if (!gl) {
+		alert("Could not initialise WebGL, sorry! :-(");
+	}        
+}
+////////////////////////////////
+function radToDeg(rad)  // added
+{
+	return rad * 180 / Math.PI;
+}
+
+function degToRad(degrees) // added
+{
+	return degrees * Math.PI / 180;
+}
+///////////////////////////////
+
+
+//----------------------------------------------------------------------------
+// Run WebGL adapted for project
+function runWebGL() {
+
+    var getUrlParameter = function getUrlParameter(sParam) {
+        var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+            sURLVariables = sPageURL.split('&'),
+            sParameterName,
+            i;
+
+        for (i = 0; i < sURLVariables.length; i++) {
+            sParameterName = sURLVariables[i].split('=');
+
+            if (sParameterName[0] === sParam) {
+                return sParameterName[1] === undefined ? true : sParameterName[1];
+            }
+        }
+    };
+ 
+	// fullscreen
+	var canvas = document.getElementById("my-canvas");
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	if(canvas.webkitRequestFullScreen) {
+		canvas.webkitRequestFullScreen();
+	}
+	else{
+		canvas.mozRequestFullScreen();
+    }
+
+	initWebGL( canvas );
+
+	shaderProgram = initShaders( gl );
+	
+	setEventListeners(canvas);
+
+	initBuffers();
+
+	//initTexture();
+	
+	initOBJ("OBJfiles/"+getUrlParameter('OBJ'));
+
+	tick();
+
+	outputInfos();
+}
+
+
